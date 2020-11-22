af9c651b56d85d3fc812c4599a6f786b9dd6def0
Tommy Tang <tommy.tang@servian.com>
2020-11-22 21:38:09 +1100

    Added the logic to derive values of public_subnet_id_for_packer

--- terraform/network/output.tf
+++ terraform/network/output.tf
@@ -9,3 +9,7 @@ output "private_subnet_ids" {
 output "public_subnet_ids" {
   value = aws_subnet.public_subnets.*.id
 }
+
+output "public_subnet_id_for_packer" {
+  value = length(aws_subnet.public_subnets.*.id) == 0 ? "" : aws_subnet.public_subnets[0].id
+}
