5a8125a325ad58d8f88456cfa8f6fddb8c2c6cb2
Tommy Tang <tommy.tang@servian.com>
2020-11-23 09:19:55 +1100

    [skip ci] Added draft README

--- terraform/network/output.tf
+++ terraform/network/output.tf
@@ -10,6 +10,8 @@ output "public_subnet_ids" {
   value = aws_subnet.public_subnets.*.id
 }
 
+# using conditional as the workaround to this issue:
+# https://github.com/terraform-google-modules/terraform-google-cloud-dns/issues/8
 output "public_subnet_id_for_packer" {
   value = length(aws_subnet.public_subnets.*.id) == 0 ? "" : aws_subnet.public_subnets[0].id
 }
