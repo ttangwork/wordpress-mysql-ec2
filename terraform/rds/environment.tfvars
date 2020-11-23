e5e2d55877019702683ed7af4618735f135dcd8c
Tommy Tang <tommy.tang@servian.com>
2020-11-24 00:06:38 +1100

    [skip ci] added provider.tf to rds

--- terraform/rds/environment.tfvars
+++ terraform/rds/environment.tfvars
@@ -1,2 +1,1 @@
-region               = "ap-southeast-2"
 app_prefix = "wordpress-mysql"
--- terraform/rds/variables.tf
+++ terraform/rds/variables.tf
@@ -1,7 +1,3 @@
-variable "region" {
-  description = "AWS Region"
-  type        = string
-}
 variable "app_prefix" {
   description = "app prefix"
   type        = string
