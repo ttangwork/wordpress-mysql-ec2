4e4b574ffe665320e91df20529f5b32b857a8df9
Tommy Tang <tommy.tang@servian.com>
2020-11-23 23:05:35 +1100

    Updated cidr_blocks values in security groups for EC2[skip ci]

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr]
   }
 
   tags = {
@@ -41,7 +41,7 @@ resource "aws_security_group" "asg_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr]
   }
 
   tags = {
