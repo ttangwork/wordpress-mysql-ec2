d48c3ea7e86172a0fa32edf3549dca09507a984e
Tommy Tang <tommy.tang@servian.com>
2020-11-24 00:03:43 +1100

    Added region variable in rds

--- terraform/rds/environment.tfvars
+++ terraform/rds/environment.tfvars
@@ -1,1 +1,2 @@
+region               = "ap-southeast-2"
 app_prefix = "wordpress-mysql"
--- terraform/rds/variables.tf
+++ terraform/rds/variables.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
 variable "app_prefix" {
   description = "app prefix"
   type        = string
