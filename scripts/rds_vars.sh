0bb2d1698dfe0c46823c531a9df55cf281a75058
Tommy Tang <tommy.tang@servian.com>
2020-11-29 09:05:04 +1100

    Added --with-decryption to get password

--- scripts/rds_vars.sh
+++ scripts/rds_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 db_user=$(aws ssm get-parameter --name wp-db-user --query 'Parameter.Value')
-db_password=$(aws ssm get-parameter --name wp-db-password --query 'Parameter.Value')
+db_password=$(aws ssm get-parameter --name wp-db-password --with-decryption --query 'Parameter.Value')
 
 echo "db_user is ${db_user}\n{db_password} is ${db_password}"
 
