8ab516e845d7e00b3f62d214abb2ce64dbf37804
Tommy Tang <tommy.tang@servian.com>
2020-11-29 09:02:21 +1100

    Added echo statements to debug

--- scripts/rds_vars.sh
+++ scripts/rds_vars.sh
@@ -2,5 +2,7 @@
 db_user=$(aws ssm get-parameter --name wp-db-user --query 'Parameter.Value')
 db_password=$(aws ssm get-parameter --name wp-db-password --query 'Parameter.Value')
 
+echo "db_user is ${db_user}\n{db_password} is ${db_password}"
+
 sed -i "s/{db_user}/${db_user}/g" ../terraform/rds/environment.tfvars
 sed -i "s/{db_password}/${db_password}/g" ../terraform/rds/environment.tfvars
