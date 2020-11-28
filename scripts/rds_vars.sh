4958dce419db6f85383f298b3ec62ce3ede794de
Tommy Tang <tommy.tang@servian.com>
2020-11-28 23:42:52 +1100

    sed e option

--- scripts/rds_vars.sh
+++ scripts/rds_vars.sh
@@ -2,5 +2,5 @@
 db_user=$(aws ssm get-parameter --name wp-db-user --query 'Parameter.Value')
 db_password=$(aws ssm get-parameter --name wp-db-password --query 'Parameter.Value')
 
-sed -i "s/{db_user}/${db_user}/g" ../terraform/rds/environment.tfvars
-sed -i "s/{db_password}/${db_password}/g" ../terraform/rds/environment.tfvars
+sed -e "s/{db_user}/${db_user}/g" ../terraform/rds/environment.tfvars
+sed -e "s/{db_password}/${db_password}/g" ../terraform/rds/environment.tfvars
