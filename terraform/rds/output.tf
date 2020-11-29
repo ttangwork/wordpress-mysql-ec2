88d66f9b8ba8b3241efa0bc3bc5016098166622e
Tommy Tang <ttang.work@gmail.com>
2020-11-29 17:47:02 +1100

    corrected the output name

--- terraform/rds/output.tf
+++ terraform/rds/output.tf
@@ -2,10 +2,6 @@ output "hostname" {
   value = aws_db_instance.rds_mysql.address
 }
 
-output "endpoint" {
-  value = aws_db_instance.rds_mysql.endpoint
-}
-
 output "dbname" {
   value = aws_db_instance.rds_mysql.name
 }
