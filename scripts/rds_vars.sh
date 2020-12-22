66e248e6f135e4d126fd5bd38d94eb182b29c00c
Tommy Tang <ttang.work@gmail.com>
2020-12-22 11:01:07 +1100

    Removed double quotes in tfvars file for db_user and db_password; Added conditional in ssm param creation

--- scripts/rds_vars.sh
+++ scripts/rds_vars.sh
@@ -9,19 +9,35 @@ aws ssm put-parameter \
 --type "String" \
 --overwrite
 
-# generating a random password
-GENERATED_PASSWORD=$(</dev/urandom tr -dc 'A-Za-z0-9#$%' | head -c 32)
+# creating ssm param for wp-db-password
+set +x
+
+
 KMS_KEY_ARN="arn:aws:kms:ap-southeast-2:461161570059:alias/wordpress_mysql_kms_key"
 
-# creating ssm param for wp-db-password
-aws ssm put-parameter \
---region "ap-southeast-2" \
---name "wp-db-password" \
---description "Database password" \
---value "${GENERATED_PASSWORD}" \
---type "SecureString" \
---key-id ${KMS_KEY_ARN} \
---overwrite
+ssm_param_check=$(aws ssm get-parameters \
+                --region "ap-southeast-2" \
+                --names "wp-db-password" \
+                --with-decryption \
+                --query "Parameters[0].Value" | tr -d '"')
+
+if [[ "$ssm_param_check" == "null" ]]; then
+    echo "Creating the password as it doesn't exist"
+    GENERATED_PASSWORD=$(</dev/urandom tr -dc 'A-Za-z0-9#$%' | head -c 32)
+
+    aws ssm put-parameter \
+    --region "ap-southeast-2" \
+    --name "wp-db-password" \
+    --description "Database password" \
+    --value "${GENERATED_PASSWORD}" \
+    --type "SecureString" \
+    --key-id ${KMS_KEY_ARN} \
+    --overwrite
+    
+    unset GENERATED_PASSWORD
+else
+    echo "The password already exists!"
+fi
 
 db_user=$(aws ssm get-parameter --name wp-db-user --query 'Parameter.Value')
 db_password=$(aws ssm get-parameter --name wp-db-password --with-decryption --query 'Parameter.Value')
@@ -29,4 +45,3 @@ db_password=$(aws ssm get-parameter --name wp-db-password --with-decryption --qu
 sed -i "s/{db_user}/${db_user}/g" ../terraform/rds/environment.tfvars
 sed -i "s/{db_password}/${db_password}/g" ../terraform/rds/environment.tfvars
 
-unset GENERATED_PASSWORD
--- terraform/rds/environment.tfvars
+++ terraform/rds/environment.tfvars
@@ -1,5 +1,5 @@
 app_prefix = "wordpress-mysql"
 db_instance_class = "db.t3.micro"
 db_name = "wordpress"
-db_user = "{db_user}"
-db_password = "{db_password}"
+db_user = {db_user}
+db_password = {db_password}
