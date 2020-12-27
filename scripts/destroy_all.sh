69df60656d7fca0f4f8afb6de664897974fa9411
Tommy Tang <ttang.work@gmail.com>
2020-12-28 09:29:56 +1100

    Added scheduled jobs

--- scripts/destroy_all.sh
+++ scripts/destroy_all.sh
@@ -2,15 +2,18 @@
 
 # Destroy EC2
 cd ../terraform/ec2
+terraform init
 terraform plan -destroy -out tfplan -var-file environment.tfvars
 terraform apply -auto-approve tfplan
 
 # Destroy RDS
 cd ../rds
+terraform init
 terraform plan -destroy -out tfplan -var-file environment.tfvars
 terraform apply -auto-approve tfplan
 
 # Destroy VPC
 cd ../network
+terraform init
 terraform plan -destroy -out tfplan -var-file environment.tfvars
 terraform apply -auto-approve tfplan
