acd8d5347dace4f7937ef9f1f4b37a2a41d8d059
Tommy Tang <ttang.work@gmail.com>
2021-01-21 14:00:30 +1100

    Updated backend for network module

--- terraform/network/backend.tf
+++ terraform/network/backend.tf
@@ -1,7 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket = "1efe223d-4333-7c13-d0dd-529f90283861-backend"
-    key    = "network.tfstate"
-    region = "ap-southeast-2"
+  backend "remote" {
+    organization = "ttangwork"
+
+    workspaces {
+      name = "wordpress-mysql-ec2"
+    }
   }
 }
